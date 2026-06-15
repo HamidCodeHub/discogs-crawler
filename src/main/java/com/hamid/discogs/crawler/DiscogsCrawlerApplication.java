43c7234e94f11f90d10b982b6e0444c4ba55919a
Hamid <hamid70yazdi@gmail.com>
2026-06-15 21:34:10 +0200

    Initial Spring Boot 4.1.0 scaffold

---
+++ src/main/java/com/hamid/discogs/crawler/DiscogsCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.hamid.discogs.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscogsCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscogsCrawlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamid/discogs/crawler/DiscogsCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamid.discogs.crawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscogsCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
